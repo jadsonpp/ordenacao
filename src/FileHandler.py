09127b386def8a87d60e6db1c828795c4d3b7495
jadsonpp <jadson_pereira5@hotmail.com>
2019-10-08 22:58:57 -0300

    att: readarqcsv

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -1,4 +1,5 @@
 import PersonHandler
+import csv
 
 #function that read a file and return a list with the data.
 def readArq(arqName):
@@ -16,3 +17,26 @@ def readArq(arqName):
                 person = PersonHandler.Person(data[0],data[1],data[2],data[3],data[4],data[5])
                 lstData.append(person)
     return lstData 
+
+#function that read a file and return a list with the data.
+def readArqCsv(arqName):
+    lstData: list = []
+    with open('./trab2-data/'+arqName,newline='') as csvFile:
+        #Split the csv file
+        data = csv.reader(csvFile,delimiter=',')
+        #jump the header
+        next(data)
+        #Transform csv in list.
+        data = list(data)
+        for row in data:
+            #Insert the data on person class.
+            person = PersonHandler.Person(data[0],data[1],data[2],data[3],data[4],data[5])
+        
+            lstData.append(person)    
+        #end for
+        #print(lstData) 
+    
+    return lstData 
+
+
+
